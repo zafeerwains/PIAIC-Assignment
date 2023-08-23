89069e54532992e819fc0fba8733bf94db80f99a
Muhammad Zafeer <zafeerwains@gmail.com>
2023-08-23 18:37:35 +0500

    First Assignment

---
+++ TS-1/index.js
@@ -0,0 +1,31 @@
+// Print a message like "Hey Abu Hurairah, I have started learning TYPESCRIPT."
+console.log("Hey Muhammad Zafeer,I have started learning Typescript");
+// Store your name in a variable and print it.
+var myName = "Muhammad Zafeer";
+console.log('My name =>', myName);
+// Store 10 numbers in different variables.
+var var1 = 1;
+var var2 = 2;
+var var3 = 3;
+var var4 = 4;
+var var5 = 5;
+var var6 = 6;
+var var7 = 7;
+var var8 = 8;
+var var9 = 9;
+var var10 = 10;
+// Add all of them and print the SUM.
+var sum = var1 + var2 + var3 + var4 + var5 + var6 + var7 + var8 + var9 + var10;
+console.log("The Sum of all varible is=>" + sum);
+// Print the Difference (subtraction).
+var sub = var1 - var2 - var3 - var4 - var5 - var6 - var7 - var8 - var9 - var10;
+console.log("The Sub of all varible is=>" + sub);
+// Print the result after multiplying all.
+var mul = var1 * var2 * var3 * var4 * var5 * var6 * var7 * var8 * var9 * var10;
+console.log("The multiple of all varible is=>" + mul);
+//Take two numbers and print the division result.
+var div = var2 / var1;
+console.log("The division of all varible is=>" + div);
+// Now, perform all four operations with the given numbers, print the results, and observe the order in which the operations take place.
+var fourOpertions = var1 / var3 + var2 * var5 / var4 - var10 * var9 + var7 + var6 / var8;
+console.log('Result of all four operations is=>', fourOpertions);
---
+++ TS-1/index.ts
@@ -0,0 +1,31 @@
+// Print a message like "Hey Abu Hurairah, I have started learning TYPESCRIPT."
+console.log("Hey Muhammad Zafeer,I have started learning Typescript")
+// Store your name in a variable and print it.
+var myName: string = "Muhammad Zafeer"
+console.log('My name =>', myName)
+// Store 10 numbers in different variables.
+var var1: number = 1
+var var2: number = 2
+var var3: number = 3
+var var4: number = 4
+var var5: number = 5
+var var6: number = 6
+var var7: number = 7
+var var8: number = 8
+var var9: number = 9
+var var10: number = 10
+// Add all of them and print the SUM.
+var sum: number = var1 + var2 + var3 + var4 + var5 + var6 + var7 + var8 + var9 + var10
+console.log("The Sum of all varible is=>" + sum)
+// Print the Difference (subtraction).
+var sub: number = var1 - var2 - var3 - var4 - var5 - var6 - var7 - var8 - var9 - var10
+console.log("The Sub of all varible is=>" + sub)
+// Print the result after multiplying all.
+var mul: number = var1 * var2 * var3 * var4 * var5 * var6 * var7 * var8 * var9 * var10
+console.log("The multiple of all varible is=>" + mul)
+//Take two numbers and print the division result.
+var div: number = var2 / var1
+console.log("The division of all varible is=>" + div)
+// Now, perform all four operations with the given numbers, print the results, and observe the order in which the operations take place.
+var fourOpertions: number = var1 / var3 + var2 * var5 / var4 - var10 * var9 + var7 + var6 / var8
+console.log('Result of all four operations is=>', fourOpertions)
