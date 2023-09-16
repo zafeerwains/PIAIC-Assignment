1d6111fece1c0893c91ba1b354919ad9e0064e5d
Muhammad Zafeer <zafeerwains@gmail.com>
2023-09-16 15:52:53 +0500

    Assignment 7 Complete  Done

--- TS-7-45 Questions/index.ts
+++ TS-7-45 Questions/index.ts
@@ -302,10 +302,34 @@ show_magicians(arrayOfMagiciains)
 
 // 42=>Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
 const make_great = (magicians: string[]) => {
-    let modified_magicians: String[]=[]
-    for (const magician of magicians) {
-        modified_magicians.push(`${magician}, The great Magician`)
-    }
+    let modified_magicians: string[] = []
+    for (const magician of magicians) modified_magicians.push(`${magician}, The great Magician`)
     return modified_magicians
 }
 console.log(make_great(arrayOfMagiciains));
+
+// 43=>Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a separate array. Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
+console.log("original array =>");
+show_magicians(arrayOfMagiciains)
+console.log("modified array =>",)
+show_magicians(make_great(arrayOfMagiciains))
+
+// 44=>Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
+const makeSandwich = (items: string[]) => {
+    console.log("You ordered a sandwich with the following items:");
+    for (const item of items) console.log(`- ${item}`);
+    console.log("Enjoy your sandwich!\n");
+}
+makeSandwich(["Ham", "Cheese", "Lettuce"]);
+makeSandwich(["Turkey", "Swiss", "Tomato", "Mayo"]);
+makeSandwich(["Peanut Butter", "Jelly", "Banana", "Honey"]);
+
+// 45=>Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
+const storeCarInfo=(manufacturer: string, modelName: string)=> {
+    const carInfo= {
+        manufacturer,
+        modelName,
+    };
+    return carInfo;
+}
+console.log(storeCarInfo("Toyota", "Camry",));
